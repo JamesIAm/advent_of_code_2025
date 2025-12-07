ea02aeb65e7b29d171a9f25979b0755cf4c81b24
James Nahajski <James.nahajski@gmail.com>
2025-12-07 16:30:46 +0000

    day5 part 2 done

--- src/day5.rs
+++ src/day5.rs
@@ -1,9 +1,11 @@
 use crate::file_utils::fetch_input;
+use std::cmp::{max, min};
+use std::collections::HashSet;
 use std::ops::Index;
 use std::str::Split;
 
 const DAY: i32 = 5;
-const TEST_ANSWER: usize = 3;
+const TEST_ANSWER: usize = 14;
 
 pub fn main() {
     let test_contents = fetch_input(DAY, true);
@@ -15,22 +17,55 @@ pub fn main() {
 }
 fn solve_day(contents: String) -> usize {
     let (ids_to_test, valid_id_ranges) = parse_input(contents);
-    println!("{:?}", ids_to_test);
-    println!("{:?}", valid_id_ranges);
-    let mut count: usize = 0;
-    for under_test in ids_to_test {
-        let mut is_valid = false;
-        for (_, (lower_bound, upper_bound)) in valid_id_ranges.iter().enumerate() {
-            if (under_test >= *lower_bound && under_test <= *upper_bound) {
-                is_valid = true;
+    let mut reduced_bounds: Vec<(usize, usize)> = Vec::new();
+    let mut change_found = true;
+    let mut current_bounds = valid_id_ranges.clone();
+    while change_found {
+        change_found = false;
+        reduced_bounds.clear();
+        for (lower_bound, upper_bound) in &current_bounds {
+            // println!("{} -> {}", lower_bound, upper_bound);
+            let mut index_to_remove: Option<usize> = Option::None;
+            let mut new_addition: (usize, usize) = (*lower_bound, *upper_bound);
+            for (index, (other_lower, other_upper)) in reduced_bounds.iter().enumerate() {
+                if other_bound_contained_in_range(lower_bound, upper_bound, other_lower, other_upper) ||
+                    other_bound_contained_in_range(other_lower, other_upper, lower_bound, upper_bound)
+                {
+                    let lowest_bound = min(*lower_bound, *other_lower);
+                    let upperest_bound = max(*upper_bound, *other_upper);
+                    index_to_remove = Some(index);
+                    new_addition = (lowest_bound, upperest_bound);
+                    change_found = true;
+                    println!(
+                        "{}-{} & {}-{} -> {}-{}",
+                        lower_bound,
+                        upper_bound,
+                        other_lower,
+                        other_upper,
+                        lowest_bound,
+                        upperest_bound
+                    );
+                    break;
+                }
             }
+            if index_to_remove.is_some() {
+                reduced_bounds.remove(index_to_remove.unwrap());
+            }
+            reduced_bounds.push(new_addition);
         }
-        if is_valid {
-            count += 1;
-            println!("Valid ID found: {}", under_test);
-        }
+        current_bounds = reduced_bounds.clone();
+        println!("Looping");
+    }
+    println!("reduced bounds: {:?}", reduced_bounds);
+    let mut count: usize = 0;
+    for (lower_bound, upper_bound) in reduced_bounds {
+        count += (upper_bound + 1) - lower_bound
     }
     count
+}
+
+fn other_bound_contained_in_range(lower: &usize, upper: &usize, other_lower: &usize, other_upper: &usize) -> bool {
+    (other_lower >= lower && other_lower <= upper) || (other_upper >= lower && other_upper <= upper)
 }
 
 fn parse_input(contents: String) -> (Vec<usize>, Vec<(usize, usize)>) {
